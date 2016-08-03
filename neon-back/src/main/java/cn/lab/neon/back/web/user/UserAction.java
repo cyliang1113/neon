750566591c57e20779b2cadaca2a66f1097691d8
cyliang1113 <cyliang1113@126.com>
2016-08-03 11:25:13 +0800

    struts

---
+++ neon-back/src/main/java/cn/lab/neon/back/web/user/UserAction.java
@@ -0,0 +1,19 @@
+package cn.lab.neon.back.web.user;
+
+import org.apache.struts2.convention.annotation.Action;
+
+import cn.lab.neon.common.BaseAction;
+
+public class UserAction extends BaseAction {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2314016829014364681L;
+	
+	@Action("/user/login")
+	public void login(){
+		responseMsg("ok");
+	}
+
+}
---
+++ neon-common/src/main/java/cn/lab/neon/common/BaseAction.java
@@ -0,0 +1,43 @@
+package cn.lab.neon.common;
+
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.Logger;
+import org.apache.struts2.ServletActionContext;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class BaseAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6943401049286581349L;
+	
+	protected Logger log = Logger.getLogger(this.getClass());
+
+	protected HttpServletResponse getResponse() {
+		return ServletActionContext.getResponse();
+	}
+
+	protected HttpServletRequest getRequest() {
+		return ServletActionContext.getRequest();
+	}
+
+	public void responseMsg(String msg) {
+		this.getResponse().setContentType("text/html;charset=UTF-8");
+		this.getResponse().setCharacterEncoding("UTF-8");
+		try {
+			PrintWriter out = this.getResponse().getWriter();
+			out.write(msg);
+			out.flush();
+			out.close();
+		} catch (Exception e) {
+			log.error(e.getMessage(), e);
+		}
+	}
+
+}
