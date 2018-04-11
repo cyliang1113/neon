4b4e88f306560eb2f4cf661432e47487ae2cec5a
cyliang1113 <cyliang1113@126.com>
2018-04-11 11:14:51 +0800

    1111

--- neon-back/src/main/java/cn/lab/neon/back/web/user/UserAction.java
+++ neon-back/src/main/java/cn/lab/neon/back/web/user/UserAction.java
@@ -1,13 +1,9 @@
 package cn.lab.neon.back.web.user;
 
 import org.apache.struts2.convention.annotation.Action;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
 import cn.lab.neon.common.BaseAction;
-import cn.lab.neon.common.jms.EmulateTopicMessageProducer;
-import cn.lab.neon.common.jms.Message;
-import cn.lab.neon.common.jms.MessageFactory;
 
 @Controller
 public class UserAction extends BaseAction {
@@ -22,11 +18,10 @@ public class UserAction extends BaseAction {
 	
 	@Action("/user/login")
 	public void login(){
-		throw new NullPointerException();
 //		Message message = MessageFactory.newUserLogginMessage(1213L, "");
 //		userLoginMessageProducer.sendMsg(message);
-		//responseMsg("ok");
-		//return;
+		responseMsg("ok");
+		return;
 	}
 
 }
