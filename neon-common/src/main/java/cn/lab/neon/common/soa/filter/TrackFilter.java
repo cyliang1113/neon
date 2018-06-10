cfcba2db6561aceea7f58bfd39f46be38417951e
cyliang1113 <cyliang1113@126.com>
2018-06-10 23:06:36 +0800

    111

---
+++ neon-common/src/main/java/cn/lab/neon/common/soa/filter/TrackFilter.java
@@ -0,0 +1,62 @@
+package cn.lab.neon.common.soa.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+
+import cn.lab.neon.common.soa.TrackNumberContext;
+import cn.lab.neon.common.soa.log.Log4JLogTailMDC;
+import cn.lab.neon.common.util.UUIDUtil;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class TrackFilter implements Filter {
+	private static final Log log = LogFactory.getLog(TrackFilter.class);
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		HttpServletRequest httpRequest = null;
+		String requestURL = null;
+		
+		try {
+			httpRequest = (HttpServletRequest) request;
+			requestURL= httpRequest.getRequestURL().toString();
+		} catch (ClassCastException e) {
+			log.error("non-HTTP request", e);
+		}
+
+		try {
+			if(TrackNumberContext.getTrackNumber() == null){
+				String trackNumber = UUIDUtil.generateUuid();
+				TrackNumberContext.setTrackNumber(trackNumber);
+			}
+			String logtail = "[trackNumber=" + TrackNumberContext.getTrackNumber() + "]";
+			Log4JLogTailMDC.setTail(logtail);
+			log.info(requestURL + " -------- start");
+			chain.doFilter(request, response);
+		} finally {
+			log.info(requestURL + " -------- end");
+			TrackNumberContext.remove();
+			Log4JLogTailMDC.removeTail();
+		}
+
+}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+}
